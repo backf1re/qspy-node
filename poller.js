8f45d857daa9e95c45bf3def5a6c0c8d75c838ad
Tim Sogard <src@timsogard.com>
2014-10-05 21:23:14 -0700

    getServers with basic mongo insert callback

--- poller.js
+++ poller.js
@@ -1,9 +1,24 @@
 var quake3 = require('./quake3');
+var mongo = require('mongoskin');
 
-quake3.getServers()
-quake3.getInfo('110.173.232.45', 27960);
+var replSet = new mongo.ReplSetServers([
+        new mongo.Server('107.170.222.53', 27017),
+        new mongo.Server('162.243.147.170', 27017),
+        new mongo.Server('104.131.135.198', 27017),
+]);
 
-//UDP message of 14 sent to master.ioquake3.org:27950
-//parseServerVars(new Buffer("sv_hostname\\dustin CPMA - CHI #1\\sv_fps\\30\\sv_maxclients\\16\\sv_minRate\\0\\sv_maxRate\\25000\\sv_minPing\\0\\sv_maxPing\\0\\sv_floodProtect\\0\\sv_allowDownload\\0\\server_gameplay\\CPM\\g_maxGameClients\\0\\version\\ioq3 1.36 linux-x86_64 Apr 12 2009\\dmflags\\0\\fraglimit\\20\\timelimit\\0\\g_gametype\\1\\protocol\\68\\mapname\\cpm22\\sv_privateClients\\0\\game\\CPMA\\gamename\\cpma\\gamedate\\Jul 27 2010\\gameversion\\1.48\\sv_arenas\\1\\Score_Time\\Warmup\\Score_Red\\0\\Score_Blue\\0\\Players_Active\\1 2 \\GTV_CN\\1\\g_needpass\\0\\mode_current\\1V1", 'ascii'));
-//41 31 "gellehsak"
-//0 35 "^rdustin"
+var db = new mongo.Db('quakespy', replSet, {w:0, native_parser: (process.env['TEST_NATIVE'] != null)});
+
+var serverColl = db.collection('quake3.servers');
+
+function insertServers(message, remote) {
+    var servers = quake3.parseGetServersReply(message, remote);
+    servers.forEach(function sToMong(elem, i, arr) {
+        serverColl.insert({host: elem[0], port: elem[1]}, function(err, result) {
+            console.log(result);
+        });
+    });
+}
+
+quake3.getServers(insertServers)
+//quake3.getInfo('110.173.232.45', 27960);
